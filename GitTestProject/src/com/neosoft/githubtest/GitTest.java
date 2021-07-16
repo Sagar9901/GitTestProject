099cdc43563d51f5ba804d12243e1b27b6da9451
Sagar9901 <panchalsagarj99@gmail.com>
2021-07-16 18:59:01 +0530

    First Commit

---
+++ GitTestProject/src/com/neosoft/githubtest/GitTest.java
@@ -0,0 +1,17 @@
+package com.neosoft.githubtest;
+
+public class GitTest {
+	public static void main(String arg[])
+	{
+		String code ="SE";
+		if(code == "SE")
+		{
+			System.out.println("Software Engineer");
+		}
+		else
+		{
+			System.out.println("Not a Software Engineer");
+		}
+	}
+
+}
